967c287c5de27af8e82c3d73c0b64010179d8d8c
Lubomir Olshansky <ol.lub.ua@gmail.com>
2018-04-19 14:33:21 +0200

    collection view added

---
+++ Cover Collection/Views/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Cover Collection
+//
+//  Created by Lubomir Olshansky on 19/04/2018.
+//  Copyright © 2018 Lubomir Olshansky. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
